72f014192c1a78186b60e9169dc76ada90c90eb9
HT <h*******t*****@gmail.com>
2012-12-26 21:00:34 -0800

    @ Changed name from ReceiptUserXXX to just UserXXX @ Modified all the references ditto

---
+++ src/com/tholix/service/ReceiptManager.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.tholix.service;
+
+import com.tholix.domain.ReceiptEntity;
+
+/**
+ * @author hitender
+ * @when Dec 26, 2012 3:09:48 PM
+ * 
+ */
+public interface ReceiptManager extends RepositoryManager<ReceiptEntity> {
+
+}
