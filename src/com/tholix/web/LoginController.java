336f06d27756f05e828761a71901a8cc17ecb9b8
HT <h*******t*****@gmail.com>
2013-05-26 17:31:49 -0700

    Making sure some way to retrieve IP address of client - Needs to be confirmed on HTTPS

--- src/com/tholix/web/LoginController.java
+++ src/com/tholix/web/LoginController.java
@@ -79,9 +79,9 @@ public class LoginController {
         if(cookies != null && cookies.length > 0) {
             Cookie cookie = cookies[0];
             String cookieId = cookie.getValue();
-            String ip = request.getRemoteAddr();
+            String ip = getClientIpAddress(request);
 
-            log.info(cookieId + ", " + ip + ", " + agent);
+            log.debug(cookieId + ", " + ip + ", " + agent);
             loginService.saveUpdateBrowserInfo(cookieId, ip, agent.toString());
         }
 
@@ -167,5 +167,34 @@ public class LoginController {
                 break;
         }
         return path;
+    }
+
+    /**
+     * Returns clients IP address
+     *
+     * @param request
+     * @return
+     */
+    public static String getClientIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if(ip == null) {
+            log.warn("IP Address found is NULL");
+        }
+        return ip;
     }
 }
