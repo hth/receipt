02e9bafbc8f919ebf156c9afe565c7052599483b
Hitender Tholia <hitender@tholix.com>
2014-01-12 00:55:30 -0800

    added end to end mileage support

--- src/com/receiptofi/web/controller/ajax/MileageWebService.java
+++ src/com/receiptofi/web/controller/ajax/MileageWebService.java
@@ -1,17 +1,28 @@
 package com.receiptofi.web.controller.ajax;
 
-import com.receiptofi.domain.UserProfileEntity;
+import com.receiptofi.domain.MileageEntity;
+import com.receiptofi.domain.UserSession;
 import com.receiptofi.service.MileageService;
 import com.receiptofi.utils.DateUtil;
 import com.receiptofi.utils.PerformanceProfiling;
-import com.receiptofi.web.controller.BaseController;
 import com.receiptofi.web.helper.json.Mileages;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +37,7 @@ import org.joda.time.DateTime;
 @Controller
 @RequestMapping(value = "/mws")
 @SessionAttributes({"userSession"})
-public class MileageWebService extends BaseController {
+public class MileageWebService {
 
     private static Logger log = LoggerFactory.getLogger(MileageWebService.class);
 
@@ -35,25 +46,68 @@ public class MileageWebService extends BaseController {
     /**
      * Helps load user mileage through ajax calls
      *
-     * @param profileId
-     * @param authKey
+     * @param userSession
+     * @param httpServletResponse
      * @return
+     * @throws IOException
      */
-    @RequestMapping(value = "/{profileId}/auth/{authKey}.json", method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/f.json", method = RequestMethod.POST, produces="application/json")
     public @ResponseBody
-    String loadJSON(@PathVariable String profileId, @PathVariable String authKey) {
+    String fetch(@ModelAttribute("userSession") UserSession userSession, HttpServletResponse httpServletResponse) throws IOException {
         DateTime time = DateUtil.now();
-        log.info("JSON : " + profileId);
 
-        UserProfileEntity userProfile = authenticate(profileId, authKey);
-        if(userProfile != null) {
-
+        if(userSession != null) {
             Mileages mileages = new Mileages();
-            mileages.setMileages(mileageService.getMileageForThisMonth(profileId, time));
+            mileages.setMileages(mileageService.getMileageForThisMonth(userSession.getUserProfileId(), time));
 
             PerformanceProfiling.log(this.getClass(), time, Thread.currentThread().getStackTrace()[1].getMethodName(), false);
             return mileages.asJson();
         } else {
+            httpServletResponse.sendError(SC_FORBIDDEN, "Cannot access directly");
+            return "{}";
+        }
+    }
+
+    @RequestMapping(value = "/m.json", method = RequestMethod.POST, produces="application/json")
+    public @ResponseBody
+    String merge(@RequestBody String ids,
+                  @ModelAttribute("userSession") UserSession userSession,
+                  HttpServletResponse httpServletResponse) throws IOException {
+
+        if(userSession != null && ids.length() > 0) {
+            try {
+                Map<String, String> map = new ObjectMapper().readValue(ids, new TypeReference<HashMap<String,String>>() {});
+                MileageEntity mileageEntity = mileageService.merge(map.get("id1"), map.get("id2"), userSession.getUserProfileId());
+                Mileages mileages = new Mileages();
+                mileages.setMileages(mileageEntity);
+                return mileages.asJson();
+            } catch(Exception exception) {
+                return "{\"success\" : false, \"message\" : \"" + exception.getLocalizedMessage() + "\"}";
+            }
+        } else {
+            httpServletResponse.sendError(SC_FORBIDDEN, "Cannot access directly");
+            return "{}";
+        }
+    }
+
+    @RequestMapping(value = "/s.json", method = RequestMethod.POST, produces="application/json")
+    public @ResponseBody
+    String split(@RequestBody String id,
+                 @ModelAttribute("userSession") UserSession userSession,
+                 HttpServletResponse httpServletResponse) throws IOException {
+
+        if(userSession != null && id.length() > 0) {
+            try {
+                Map<String, String> map = new ObjectMapper().readValue(id, new TypeReference<HashMap<String,String>>() {});
+                List<MileageEntity> mileageEntities = mileageService.split(map.get("id"), userSession.getUserProfileId());
+                Mileages mileages = new Mileages();
+                mileages.setMileages(mileageEntities);
+                return mileages.asJson();
+            } catch(Exception exception) {
+                return "{\"success\" : false, \"message\" : \"" + exception.getLocalizedMessage() + "\"}";
+            }
+        } else {
+            httpServletResponse.sendError(SC_FORBIDDEN, "Cannot access directly");
             return "{}";
         }
     }
