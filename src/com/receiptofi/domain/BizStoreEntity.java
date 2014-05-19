c8def9f712327b0f68f041ad98494429bf275b73
Hitender Tholia <hitender@tholix.com>
2014-05-18 20:36:00 -0700

    optimized imports

--- src/com/receiptofi/domain/BizStoreEntity.java
+++ src/com/receiptofi/domain/BizStoreEntity.java
@@ -1,8 +1,6 @@
 package com.receiptofi.domain;
 
 import com.receiptofi.utils.Formatter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
 
--- src/com/receiptofi/web/controller/access/UserProfilePreferenceController.java
+++ src/com/receiptofi/web/controller/access/UserProfilePreferenceController.java
@@ -4,9 +4,9 @@
 package com.receiptofi.web.controller.access;
 
 import com.receiptofi.domain.ExpenseTagEntity;
-import com.receiptofi.domain.site.ReceiptUser;
 import com.receiptofi.domain.UserPreferenceEntity;
 import com.receiptofi.domain.UserProfileEntity;
+import com.receiptofi.domain.site.ReceiptUser;
 import com.receiptofi.service.AccountService;
 import com.receiptofi.service.ItemService;
 import com.receiptofi.service.UserProfilePreferenceService;
