c253776cdd5baea6819bdbeafb18f2bc0dcedbfd
Hitender Tholia <hitender@tholix.com>
2014-05-18 18:15:06 -0700

    update log message when user is not found

--- src/com/receiptofi/service/CustomUserDetailsService.java
+++ src/com/receiptofi/service/CustomUserDetailsService.java
@@ -56,7 +56,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                     userAccountEntity.isActive() && userAccountEntity.isAccountValidated()
             );
         } else {
-            log.error("Error in retrieving user");
+            log.error("not found user={}", email);
             throw new UsernameNotFoundException("Error in retrieving user");
         }
     }
@@ -80,7 +80,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                     userAccountEntity.isActive() && userAccountEntity.isAccountValidated()
             );
         } else {
-            log.error("Error in retrieving user");
+            log.error("not found user={}", uid);
             throw new UsernameNotFoundException("Error in retrieving user");
         }
     }
