021fdd65ec72629e29725cdb3d760d49027e02a6
Hitender Tholia <hitender@tholix.com>
2014-05-26 13:10:36 -0700

    allow redis port for logstash

--- tools/firewall/ipfw.receiptofi.startup.sh
+++ tools/firewall/ipfw.receiptofi.startup.sh
@@ -1,2 +1,3 @@
 sudo ipfw add 100 fwd 127.0.0.1,8080 tcp from any to any 80 in
 sudo ipfw add 110 fwd 127.0.0.1,8443 tcp from any to any 443 in
+sudo ipfw add 120 allow tcp from 192.168.1.74 to any dst-port 6379
