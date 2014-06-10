a29f16659a601b5d41a5c3a17c0bb2e6bb795742
Hitender Tholia <hitender@tholix.com>
2014-06-10 03:38:12 -0700

    Dealing with “java.lang.OutOfMemoryError: PermGen space” error

--- tools/tomcat/bin/setenv.sh
+++ tools/tomcat/bin/setenv.sh
@@ -1,4 +1,4 @@
 export CATALINA_HOME=/Library/Tomcat
 export JAVA_HOME=/Library/Java/Home
-export CATALINA_OPTS="-Xms1024M -Xmx4096M"
+export CATALINA_OPTS="-Xms1024M -Xmx4096M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
 export CATALINA_PID=/tmp/catalina.pid
