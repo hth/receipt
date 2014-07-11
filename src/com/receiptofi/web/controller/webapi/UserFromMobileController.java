a569a3fd24237322f0b58f70cc4aada7d0c2bfe3
Hitender Tholia <hitender@tholix.com>
2014-07-11 00:54:33 -0700

    changed package to match PATH

--- src/com/receiptofi/web/controller/webapi/UserFromMobileController.java
+++ src/com/receiptofi/web/controller/webapi/UserFromMobileController.java
@@ -1,4 +1,4 @@
-package com.receiptofi.web.controller.api;
+package com.receiptofi.web.controller.webapi;
 
 import com.receiptofi.domain.types.ProviderEnum;
 import com.receiptofi.service.CustomUserDetailsService;
@@ -25,8 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 /**
