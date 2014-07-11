65a7f6d30af8856124a60abe668a451fcd01cbeb
Hitender Tholia <hitender@tholix.com>
2014-07-11 01:58:06 -0700

    removed from server as its shared with every server; made sure its just in CATALINA_OPTS

--- tools/tomcat/bin/setenv.sh
+++ tools/tomcat/bin/setenv.sh
@@ -1,8 +1,7 @@
 export JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8
--server -Xms1536m -Xmx2560m
 -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m
 -XX:MaxPermSize=256m -XX:+DisableExplicitGC"
 export CATALINA_HOME=/Library/Tomcat
 export JAVA_HOME=/Library/Java/Home
-export CATALINA_OPTS="-Xms1024M -Xmx4096M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
+export CATALINA_OPTS="-Xms2048M -Xmx4096M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
 export CATALINA_PID=/tmp/catalina.pid
