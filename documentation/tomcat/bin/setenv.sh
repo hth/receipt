38456a06af1f3341664eaf1a3342088cfc9fd0a1
Hitender Tholia <hitender@tholix.com>
2014-10-22 13:34:39 -0700

    added -XX:MaxPermSize=512M -XX:+UserParNewGC -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote"

--- documentation/tomcat/bin/setenv.sh
+++ documentation/tomcat/bin/setenv.sh
@@ -1,9 +1,15 @@
-export JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8
--XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m
--XX:MaxPermSize=256m -XX:+DisableExplicitGC
--XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode
--Dcom.sun.management.jmxremote"
+#!/bin/sh
+export JAVA_OPTS="
+-Djava.awt.headless=true
+-Dfile.encoding=UTF-8
+-XX:NewSize=256m
+-XX:MaxNewSize=256m
+-XX:PermSize=256m
+-XX:MaxPermSize=512m
+-XX:+DisableExplicitGC
+-XX:+UseConcMarkSweepGC
+-XX:+CMSIncrementalMode"
 export CATALINA_HOME=/Library/Tomcat
 export JAVA_HOME=/Library/Java/Home
-export CATALINA_OPTS="-Xms2048M -Xmx4096M -XX:+CMSClassUnloadingEnabled"
+export CATALINA_OPTS="-Xms2G -Xmx6G -XX:MaxPermSize=512M -XX:+UserParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dcom.sun.management.jmxremote"
 export CATALINA_PID=/tmp/catalina.pid
