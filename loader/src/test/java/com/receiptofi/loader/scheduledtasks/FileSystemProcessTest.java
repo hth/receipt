230eb46b06eeb815c6c649db4964defb5248d29d
Hitender Tholia <hitender@tholix.com>
2014-10-24 02:43:02 -0700

    added new test

---
+++ loader/src/test/java/com/receiptofi/loader/scheduledtasks/FileSystemProcessTest.java
@@ -0,0 +1,39 @@
+package com.receiptofi.loader.scheduledtasks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileSystemProcessTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveExpiredExcelFiles() throws Exception {
+
+    }
+
+    @Test
+    public void testGetExcelFile() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveExpiredExcel() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveExpiredExcel1() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveTempFiles() throws Exception {
+
+    }
+}
