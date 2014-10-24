be731a24bf4fa42212af3e10a095fad539082305
Hitender Tholia <hitender@tholix.com>
2014-10-23 18:57:34 -0700

    added unit test for purge Rejected documents

---
+++ loader/src/test/java/com/receiptofi/loader/scheduledtasks/PurgeDocumentsProcessTest.java
@@ -0,0 +1,49 @@
+package com.receiptofi.loader.scheduledtasks;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+import com.receiptofi.domain.DocumentEntity;
+import com.receiptofi.repository.DocumentManager;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class PurgeDocumentsProcessTest {
+
+    private PurgeDocumentsProcess purgeDocumentsProcess;
+    @Mock private DocumentManager documentManager;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void whenPurgeIsTurnedOn() throws Exception {
+        purgeDocumentsProcess = new PurgeDocumentsProcess(1, 1, "ON", documentManager);
+        when(documentManager.getAllRejected(1)).thenReturn(Arrays.asList(new DocumentEntity(), new DocumentEntity()));
+        purgeDocumentsProcess.purgeRejectedDocument();
+        verify(documentManager, atMost(1)).deleteHard(any(DocumentEntity.class));
+    }
+
+    @Test
+    public void whenPurgeIsTurnedOff() throws Exception {
+        purgeDocumentsProcess = new PurgeDocumentsProcess(1, 1, "OFF", documentManager);
+        when(documentManager.getAllRejected(1)).thenReturn(Arrays.asList(new DocumentEntity(), new DocumentEntity()));
+        purgeDocumentsProcess.purgeRejectedDocument();
+        verify(documentManager, never()).deleteHard(any(DocumentEntity.class));
+    }
+
+    @Test
+    public void purgeAll() throws Exception {
+        purgeDocumentsProcess = new PurgeDocumentsProcess(1, -1, "ON", documentManager);
+        when(documentManager.getAllRejected(1)).thenReturn(Arrays.asList(new DocumentEntity(), new DocumentEntity()));
+        purgeDocumentsProcess.purgeRejectedDocument();
+        verify(documentManager, atMost(2)).deleteHard(any(DocumentEntity.class));
+    }
+}
