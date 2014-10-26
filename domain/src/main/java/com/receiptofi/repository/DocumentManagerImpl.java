e08eee3ddc81814f0bfc7dcae1f66d6b31629c5f
Hitender Tholia <tholiahitender@gmail.com>
2014-10-26 00:56:52 -0700

    removed unused import

--- domain/src/main/java/com/receiptofi/repository/DocumentManagerImpl.java
+++ domain/src/main/java/com/receiptofi/repository/DocumentManagerImpl.java
@@ -26,8 +26,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.WriteResultChecking;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
