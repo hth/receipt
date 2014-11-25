83d5a3ea6ba70318edaac9405b336d08909e28f4
Hitender Tholia <tholiahitender@gmail.com>
2014-11-24 18:23:39 -0800

    fixed log format

--- web/src/main/java/com/receiptofi/web/controller/access/MileageOdometerController.java
+++ web/src/main/java/com/receiptofi/web/controller/access/MileageOdometerController.java
@@ -48,7 +48,7 @@ public final class MileageOdometerController {
 
     @RequestMapping (value = "/{mileageId}", method = RequestMethod.GET)
     public ModelAndView loadForm(
-            @PathVariable
+            @PathVariable ("mileageId")
             String mileageId,
 
             @ModelAttribute ("mileageForm")
@@ -57,7 +57,7 @@ public final class MileageOdometerController {
             Model model
     ) {
         DateTime time = DateUtil.now();
-        LOG.info("Loading MileageEntity with id: " + mileageId);
+        LOG.info("Loading mileage id={}", mileageId);
 
         ReceiptUser receiptUser = (ReceiptUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -95,7 +95,7 @@ public final class MileageOdometerController {
             RedirectAttributes redirectAttrs
     ) {
         DateTime time = DateUtil.now();
-        LOG.info("Delete mileage " + mileageForm.getMileage().getId());
+        LOG.info("Delete mileage id={}", mileageForm.getMileage().getId());
 
         ReceiptUser receiptUser = (ReceiptUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -138,7 +138,7 @@ public final class MileageOdometerController {
             RedirectAttributes redirectAttrs
     ) {
         DateTime time = DateUtil.now();
-        LOG.debug("Split mileage " + mileageForm.getMileage().getId());
+        LOG.debug("Split mileage id={}", mileageForm.getMileage().getId());
         ReceiptUser receiptUser = (ReceiptUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         try {
             mileageService.split(mileageForm.getMileage().getId(), receiptUser.getRid());
