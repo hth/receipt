708010afcf3e5b8f74752a135d38a607c7cc8822
Hitender Tholia <tholiahitender@gmail.com>
2014-11-26 17:13:23 -0800

    PMD fix

--- domain/src/main/java/com/receiptofi/repository/ItemManagerImpl.java
+++ domain/src/main/java/com/receiptofi/repository/ItemManagerImpl.java
@@ -83,7 +83,7 @@ public final class ItemManagerImpl implements ItemManager {
     }
 
     @Override
-    public void saveObjects(List<ItemEntity> objects) throws Exception {
+    public void saveObjects(List<ItemEntity> objects) {
         mongoTemplate.setWriteResultChecking(WriteResultChecking.LOG);
         try {
             //TODO reflection error saving the list
@@ -95,8 +95,8 @@ public final class ItemManagerImpl implements ItemManager {
                 sequence++;
             }
         } catch (DataIntegrityViolationException e) {
-            LOG.error("Duplicate record entry for ItemEntity: " + e.getLocalizedMessage());
-            throw new Exception(e.getMessage());
+            LOG.error("Duplicate record entry for ItemEntity reason={}", e.getLocalizedMessage(), e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -162,13 +162,10 @@ public final class ItemManagerImpl implements ItemManager {
         // Can choose Item create date but if needs accuracy then find receipts for these items and filter
         // receipts by date provided.
         // Not sure how much beneficial it would be other than more data crunching.
-        Criteria criteriaA = where("IN").is(name);
-        Query query = query(criteriaA);
+        Query query = query(where("IN").is(name));
 
-        Criteria criteriaB;
-        if (receiptUserId != null) {
-            criteriaB = where("RID").is(receiptUserId);
-            query = query(criteriaA.andOperator(criteriaB.andOperator(isNotDeleted())));
+        if (null != receiptUserId) {
+            query.addCriteria(where("RID").is(receiptUserId).andOperator(isNotDeleted()));
         }
 
         return mongoTemplate.find(query, ItemEntity.class, TABLE);
@@ -211,9 +208,10 @@ public final class ItemManagerImpl implements ItemManager {
 
     @Override
     public WriteResult updateObject(ItemEntity object) {
-        Query query = query(where("id").is(object.getId()));
-        Update update = Update.update("IN", object.getName());
-        return mongoTemplate.updateFirst(query, entityUpdate(update), TABLE);
+        return mongoTemplate.updateFirst(
+                query(where("id").is(object.getId())),
+                entityUpdate(Update.update("IN", object.getName())),
+                TABLE);
     }
 
     @Override
