1fe7df27b414ce1ebad2a14a45b0260afe62bad4
Hitender Tholia <tholiahitender@gmail.com>
2014-12-01 04:04:02 -0800

    added missing test

--- domain/src/main/java/com/receiptofi/repository/StorageManagerImpl.java
+++ domain/src/main/java/com/receiptofi/repository/StorageManagerImpl.java
@@ -20,9 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.util.Assert;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
 import java.util.List;
 
