33728dc2aa6cf6fde9774e546f578a6f7b4beaf0
Hitender Tholia <tholiahitender@gmail.com>
2014-11-30 17:24:09 -0800

    renamed outputStream

--- web/src/main/java/com/receiptofi/web/controller/access/FileDownloadController.java
+++ web/src/main/java/com/receiptofi/web/controller/access/FileDownloadController.java
@@ -93,10 +93,10 @@ public final class FileDownloadController {
                 setContentType(file.getName(), response);
                 response.setHeader("Content-Length", String.valueOf(file.length()));
                 response.setHeader("Content-Disposition", "inline; filename=" + file.getName());
-                OutputStream out = response.getOutputStream();
+                OutputStream outputStream = response.getOutputStream();
                 /** imageNotFound is of type gif */
-                ImageIO.write(bi, FileUtil.getFileExtension(file.getName()), out);
-                out.close();
+                ImageIO.write(bi, FileUtil.getFileExtension(file.getName()), outputStream);
+                outputStream.close();
             } else {
                 LOG.debug("Length={} MetaData={}", gridFSDBFile.getLength(), gridFSDBFile.getMetaData());
                 setContentType(gridFSDBFile.getFilename(), response);
