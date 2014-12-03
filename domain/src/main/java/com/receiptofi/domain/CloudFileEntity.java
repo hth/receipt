30d788a057c4c0c6e5ff6a19e7903f986992b73c
Hitender Tholia <tholiahitender@gmail.com>
2014-12-03 04:19:31 -0800

    Added collection to delete files in cloud

---
+++ domain/src/main/java/com/receiptofi/domain/CloudFileEntity.java
@@ -0,0 +1,43 @@
+package com.receiptofi.domain;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * User: hitender
+ * Date: 12/2/14 6:17 PM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@Document (collection = "CLOUD_FILES")
+public class CloudFileEntity extends BaseEntity {
+
+    @NotNull
+    @Field ("BID")
+    private String blobId;
+
+    private CloudFileEntity(String blobId) {
+        this.blobId = blobId;
+    }
+
+    public static CloudFileEntity newInstance(String blobId) {
+        return new CloudFileEntity(blobId);
+    }
+
+    public String getBlobId() {
+        return blobId;
+    }
+
+    @Override
+    public String toString() {
+        return "CloudFilesEntity{" +
+                "blobId='" + blobId + '\'' +
+                '}';
+    }
+}
