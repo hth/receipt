b831c80e1e4ed30a7bf24be6ca4b58bd4c758614
Hitender Tholia <tholiahitender@gmail.com>
2014-12-04 19:11:01 -0800

    fixed test

--- loader/src/test/java/com/receiptofi/loader/service/AmazonS3ServiceTest.java
+++ loader/src/test/java/com/receiptofi/loader/service/AmazonS3ServiceTest.java
@@ -8,7 +8,6 @@ import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.springframework.util.Assert;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -17,13 +16,17 @@ import java.io.FileReader;
 import java.util.Arrays;
 import java.util.Properties;
 
+/**
+ * For this test it is imperative to have properties files in place before executing any test.
+ * Based on environment profile, appropriate properties files are copied under $build/$explodedWar.
+ * For this reason build.gradle file has task called $setUpProperties.
+ */
 @SuppressWarnings ({
         "PMD.BeanMembersShouldSerialize",
         "PMD.LocalVariableCouldBeFinal",
         "PMD.MethodArgumentCouldBeFinal",
         "PMD.LongVariable"
 })
-@Ignore
 public class AmazonS3ServiceTest {
     public static final String BUILD = "receipt" +
             File.separator +
@@ -38,32 +41,25 @@ public class AmazonS3ServiceTest {
             File.separator +
             "conf";
 
+    private final FileFilter profileF = new WildcardFileFilter(Arrays.asList("dev", "test"));
+    private final FileFilter propertiesF = new WildcardFileFilter(Arrays.asList("dev.properties", "test.properties"));
+    private final Properties prop = new Properties();
     private AmazonS3Service amazonS3Service;
-    private FileFilter profile = new WildcardFileFilter(Arrays.asList("dev", "test"));
-    private FileFilter properties = new WildcardFileFilter(Arrays.asList("dev.properties", "test.properties"));
-    private Properties p = new Properties();
 
     @Before
     public void setUp() throws Exception {
-        if (p.keySet().isEmpty()) {
+        if (prop.keySet().isEmpty()) {
             ClassLoader classLoader = AmazonS3ServiceTest.class.getClassLoader();
-            File[] profileDir = findFiles(classLoader.getResource("").getPath().split("receipt")[0] + BUILD, profile);
-            File[] propertiesFiles = findFiles(profileDir[0].getAbsolutePath() + CONF, properties);
-            p.load(new FileReader(propertiesFiles[0]));
+            File[] profileDir = findFiles(classLoader.getResource("").getPath().split("receipt")[0] + BUILD, profileF);
+            File[] propertiesFiles = findFiles(profileDir[0].getAbsolutePath() + CONF, propertiesF);
+            prop.load(new FileReader(propertiesFiles[0]));
         }
 
         amazonS3Service = new AmazonS3Service(
-                p.getProperty("aws.s3.accessKey"),
-                p.getProperty("aws.s3.secretKey"),
-                p.getProperty("aws.s3.bucketName")
+                prop.getProperty("aws.s3.accessKey"),
+                prop.getProperty("aws.s3.secretKey"),
+                prop.getProperty("aws.s3.bucketName")
         );
-    }
-
-    private File[] findFiles(String location, FileFilter fileFilter) {
-        File directory = new File(location);
-        File[] files = directory.listFiles(fileFilter);
-        Assert.isTrue(files.length == 1);
-        return files;
     }
 
     @Test
@@ -73,6 +69,13 @@ public class AmazonS3ServiceTest {
 
     @Test
     public void testIfBucketExists() {
-        assertTrue("bucket exists", amazonS3Service.getS3client().doesBucketExist("chk.test"));
+        assertTrue("exists", amazonS3Service.getS3client().doesBucketExist(prop.getProperty("aws.s3.bucketName")));
+    }
+
+    private File[] findFiles(String location, FileFilter fileFilter) {
+        File directory = new File(location);
+        File[] files = directory.listFiles(fileFilter);
+        Assert.isTrue(files.length == 1);
+        return files;
     }
 }
