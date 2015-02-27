6f9fb4615a64117e5a38a74a3ed3075b63b05b58
Hitender Tholia <tholiahitender@gmail.com>
2015-02-27 15:12:38 -0800

    added item analysis

--- web/src/main/java/com/receiptofi/web/controller/access/ItemAnalyticController.java
+++ web/src/main/java/com/receiptofi/web/controller/access/ItemAnalyticController.java
@@ -43,7 +43,7 @@ import java.util.List;
 public class ItemAnalyticController {
     private static final Logger LOG = LoggerFactory.getLogger(ItemAnalyticController.class);
 
-    @Value ("${ItemAnalyticController.nextPage:/itemanalytic}")
+    @Value ("${ItemAnalyticController.nextPage:/itemanalytic2}")
     private String nextPage;
 
     @Value ("${ItemAnalyticController.searchLimitForDays:90}")
