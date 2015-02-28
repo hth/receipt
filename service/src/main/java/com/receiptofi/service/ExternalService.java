e4fce0fef43356ed94e20dfc2e92aaae160734cc
Hitender Tholia <tholiahitender@gmail.com>
2015-02-28 13:53:10 -0800

    removed temp code and GeoApiContext

--- service/src/main/java/com/receiptofi/service/ExternalService.java
+++ service/src/main/java/com/receiptofi/service/ExternalService.java
@@ -3,16 +3,12 @@ package com.receiptofi.service;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
-import com.google.maps.model.GeocodingResult;
 
 import com.receiptofi.domain.BizStoreEntity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -38,23 +34,14 @@ import java.util.Iterator;
 @Service
 public class ExternalService {
     private static final Logger LOG = LoggerFactory.getLogger(ExternalService.class);
-
-    private boolean invokeGoogleMapJavaApi;
-    private GeoApiContext context;
-
     private static final String ADDRESS_DECODE_URL = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=";
 
-    @Autowired
-    public ExternalService(
-            @Value("${google-browser-api-key}")
-            String googleBrowserApiKey,
+    @Value ("${google-browser-api-key}")
+    private String googleBrowserApiKey;
 
-            @Value("${invokeGoogleMapJavaApi:false}")
-            boolean invokeGoogleMapJavaApi
-    ) {
-        this.context = new GeoApiContext().setApiKey(googleBrowserApiKey);
-        this.invokeGoogleMapJavaApi = invokeGoogleMapJavaApi;
-    }
+    @Value ("${invokeGoogleMapJavaApi:false}")
+    private boolean invokeGoogleMapJavaApi;
+    //Look into GeoApiContext and GeocodingApi
 
     /**
      * Finds Latitude and Longitude for a given address from Google Web Service.
@@ -63,32 +50,27 @@ public class ExternalService {
      * @throws Exception
      */
     public void decodeAddress(BizStoreEntity bizStoreEntity) throws IOException {
-        if(invokeGoogleMapJavaApi) {
-            //TODO move towards JAVA API
-            populateBizStore(bizStoreEntity.getAddress(), bizStoreEntity);
-        } else {
-            URL url = null;
-            try {
-                String encodeAddress = URLEncoder.encode(bizStoreEntity.getAddress(), "UTF-8");
-                url = new URL(ADDRESS_DECODE_URL + encodeAddress);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        URL url = null;
+        try {
+            String encodeAddress = URLEncoder.encode(bizStoreEntity.getAddress(), "UTF-8");
+            url = new URL(ADDRESS_DECODE_URL + encodeAddress);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
-                StringBuilder output = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    output.append(line);
-                }
-                reader.close();
-
-                populateBizStore(output, bizStoreEntity);
-            } catch (MalformedURLException e) {
-                String result = (null == url) ? bizStoreEntity.getAddress() : url.toString() + ", " + bizStoreEntity.getAddress();
-                LOG.error("URL: " + result + ", " + e.getLocalizedMessage());
-                throw new MalformedURLException("URL: " + result + ", " + e.getLocalizedMessage());
-            } catch (IOException e) {
-                String result = (null == url) ? bizStoreEntity.getAddress() : url.toString() + ", " + bizStoreEntity.getAddress();
-                throw new IOException("URL: " + result + ", " + e.getLocalizedMessage());
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
             }
+            reader.close();
+
+            populateBizStore(output, bizStoreEntity);
+        } catch (MalformedURLException e) {
+            String result = (null == url) ? bizStoreEntity.getAddress() : url.toString() + ", " + bizStoreEntity.getAddress();
+            LOG.error("URL: " + result + ", " + e.getLocalizedMessage());
+            throw new MalformedURLException("URL: " + result + ", " + e.getLocalizedMessage());
+        } catch (IOException e) {
+            String result = (null == url) ? bizStoreEntity.getAddress() : url.toString() + ", " + bizStoreEntity.getAddress();
+            throw new IOException("URL: " + result + ", " + e.getLocalizedMessage());
         }
     }
 
@@ -117,17 +99,6 @@ public class ExternalService {
             String formattedAddress = formattedAddressElement.getAsString();
             bizStoreEntity.setAddress(formattedAddress);
             bizStoreEntity.setValidatedUsingExternalAPI(true);
-        }
-    }
-
-    private void populateBizStore(String address, BizStoreEntity bizStoreEntity) {
-        try {
-            GeocodingResult[] results =  GeocodingApi.geocode(context, address).await();
-            String formattedAddress = results[0].formattedAddress;
-            bizStoreEntity.setValidatedUsingExternalAPI(true);
-        } catch (Exception e) {
-            LOG.error("Failed to get address from google maps service, reason={}",
-                    e.getLocalizedMessage(), e);
         }
     }
 }
