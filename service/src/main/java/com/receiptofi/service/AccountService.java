6451c4266f2050f374df3e775a3a5c778c86f960
Hitender Tholia <tholiahitender@gmail.com>
2015-03-22 16:57:52 -0700

    removed unused import

--- service/src/main/java/com/receiptofi/service/AccountService.java
+++ service/src/main/java/com/receiptofi/service/AccountService.java
@@ -32,7 +32,6 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
