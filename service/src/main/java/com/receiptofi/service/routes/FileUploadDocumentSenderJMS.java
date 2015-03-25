f5b4acdd27d0b69aa05670558971e91a73841092
Hitender Tholia <tholiahitender@gmail.com>
2015-03-25 00:26:59 -0700

    import clean up

--- service/src/main/java/com/receiptofi/service/routes/FileUploadDocumentSenderJMS.java
+++ service/src/main/java/com/receiptofi/service/routes/FileUploadDocumentSenderJMS.java
@@ -12,13 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
 import javax.jms.MapMessage;
-import javax.jms.Message;
-import javax.jms.Session;
 
 /**
  * @author hitender
