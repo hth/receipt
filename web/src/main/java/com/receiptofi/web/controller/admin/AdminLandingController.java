9db61d2faa3df0447886381f7977f7999cb0597c
Hitender Tholia <tholiahitender@gmail.com>
2015-03-30 12:42:14 -0700

    added SearchUser

--- web/src/main/java/com/receiptofi/web/controller/admin/AdminLandingController.java
+++ web/src/main/java/com/receiptofi/web/controller/admin/AdminLandingController.java
@@ -3,24 +3,18 @@
  */
 package com.receiptofi.web.controller.admin;
 
-import com.receiptofi.domain.UserProfileEntity;
-import com.receiptofi.service.AdminLandingService;
+import com.receiptofi.domain.site.ReceiptUser;
 import com.receiptofi.web.form.UserSearchForm;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 /**
  * Redirect to prevent re-submit.
@@ -42,8 +36,6 @@ public class AdminLandingController {
     @Value ("${nextPage:/admin/landing}")
     private String nextPage;
 
-    @Autowired private AdminLandingService adminLandingService;
-
     /**
      * Gymnastic for PRG example.
      *
@@ -55,39 +47,8 @@ public class AdminLandingController {
             @ModelAttribute ("userSearchForm")
             UserSearchForm userSearchForm
     ) {
+        ReceiptUser receiptUser = (ReceiptUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        LOG.info("Landed on admin page rid={}", receiptUser.getRid());
         return nextPage;
-    }
-
-    /**
-     * @param name Search for user name.
-     * @return
-     */
-    @RequestMapping (value = "/find_user", method = RequestMethod.GET)
-    @ResponseBody
-    public List<String> findUser(
-            @RequestParam ("term")
-            String name
-    ) {
-        return adminLandingService.findMatchingUsers(name);
-    }
-
-    /**
-     * Gymnastic for PRG example.
-     *
-     * @param userSearchForm
-     * @return
-     */
-    @RequestMapping (value = "/landing", method = RequestMethod.POST)
-    public String loadUser(
-            @ModelAttribute ("userSearchForm")
-            UserSearchForm userSearchForm,
-
-            RedirectAttributes redirectAttrs
-    ) {
-        List<UserProfileEntity> userProfileEntities = adminLandingService.findAllUsers(userSearchForm.getUserName());
-        userSearchForm.setUserProfiles(userProfileEntities);
-        redirectAttrs.addFlashAttribute("userSearchForm", userSearchForm);
-        /** Re-direct to prevent resubmit. */
-        return "redirect:" + nextPage + ".htm";
     }
 }
