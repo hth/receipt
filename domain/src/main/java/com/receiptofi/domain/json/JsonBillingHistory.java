0185397ecb8d2a3f2600e69a030aab1a87debe69
Hitender Tholia <tholiahitender@gmail.com>
2015-04-19 15:55:40 -0700

    added billing json

---
+++ domain/src/main/java/com/receiptofi/domain/json/JsonBillingHistory.java
@@ -0,0 +1,51 @@
+package com.receiptofi.domain.json;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import com.receiptofi.domain.BillingHistoryEntity;
+import com.receiptofi.domain.annotation.Mobile;
+
+/**
+ * User: hitender
+ * Date: 4/19/15 3:22 PM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable",
+        "unused"
+})
+@JsonAutoDetect (
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder (alphabetic = true)
+@JsonIgnoreProperties (ignoreUnknown = true)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+@Mobile
+public class JsonBillingHistory {
+
+    @JsonProperty ("rid")
+    private String receiptUserId;
+
+    @JsonProperty ("bs")
+    private String billedStatus;
+
+    @JsonProperty ("bt")
+    private String accountBillingType;
+
+    @JsonProperty ("bm")
+    private String billedForMonth;
+
+    public JsonBillingHistory(BillingHistoryEntity billingHistory) {
+        this.receiptUserId = billingHistory.getRid();
+        this.billedStatus = billingHistory.getBilledStatus().name();
+        this.accountBillingType = billingHistory.getAccountBillingType().name();
+        this.billedForMonth = billingHistory.getBilledForMonthYear();
+    }
+}
