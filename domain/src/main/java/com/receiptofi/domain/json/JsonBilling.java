6afeeb62416a1d2f111367b9aaccefbfb0ef8880
Hitender Tholia <tholiahitender@gmail.com>
2015-07-08 18:04:32 -0700

    added back jackson-lib and log

--- domain/src/main/java/com/receiptofi/domain/json/JsonBilling.java
+++ domain/src/main/java/com/receiptofi/domain/json/JsonBilling.java
@@ -10,6 +10,11 @@ import com.receiptofi.domain.BillingHistoryEntity;
 import com.receiptofi.domain.annotation.Mobile;
 import com.receiptofi.domain.types.BillingPlanEnum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.util.Assert;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,6 +40,7 @@ import java.util.stream.Collectors;
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 @Mobile
 public class JsonBilling {
+    private static final Logger LOG = LoggerFactory.getLogger(JsonBilling.class);
 
     @JsonProperty ("bt")
     private String billingPlan;
@@ -46,6 +52,8 @@ public class JsonBilling {
         //TODO(hth) fix this issue as there should be a default NB status for all account
         /** BillingAccount can be inactive when user has un-subscribed. */
         if (billingAccount == null) {
+            Assert.notEmpty(billings, "Billing Histories are empty");
+            LOG.error("Billing Account is null for rid=", billings.get(0).getRid());
             this.billingPlan = BillingPlanEnum.NB.name();
         } else {
             this.billingPlan = billingAccount.getBillingPlan().name();
