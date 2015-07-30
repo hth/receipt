5e782f1d6093b37c080c7c7b5ba7436c95de8c23
hth <hth@users.noreply.github.com>
2015-07-30 07:42:37 +0000

    Update setenv.sh

--- documentation/tomcat/bin/setenv.sh
+++ documentation/tomcat/bin/setenv.sh
@@ -4,12 +4,11 @@ export JAVA_OPTS="
 -Dfile.encoding=UTF-8
 -XX:NewSize=256m
 -XX:MaxNewSize=256m
--XX:PermSize=256m
--XX:MaxPermSize=512m
+-XX:MaxMetaspaceSize=512m
 -XX:+DisableExplicitGC
 -XX:+UseConcMarkSweepGC
 -XX:+CMSIncrementalMode"
 export CATALINA_HOME=/Library/Tomcat
 export JAVA_HOME=/Library/Java/Home
-export CATALINA_OPTS="-Xms2G -Xmx6G -XX:MaxPermSize=512M -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dcom.sun.management.jmxremote"
+export CATALINA_OPTS="-Xms2G -Xmx6G -XX:MaxMetaspaceSize=512M -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Dcom.sun.management.jmxremote"
 export CATALINA_PID=/tmp/catalina.pid
