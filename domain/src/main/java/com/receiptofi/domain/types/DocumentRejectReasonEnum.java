321b0708c9794ef8c801e270a357385881c6d7a4
Hitender Tholia <tholiahitender@gmail.com>
2015-09-13 20:33:02 -0700

    updated in sentence for better information for user

--- domain/src/main/java/com/receiptofi/domain/types/DocumentRejectReasonEnum.java
+++ domain/src/main/java/com/receiptofi/domain/types/DocumentRejectReasonEnum.java
@@ -5,12 +5,12 @@ package com.receiptofi.domain.types;
  * Date: 9/12/15 1:50 PM
  */
 public enum DocumentRejectReasonEnum {
-    C("C", "Not clear", "is not clear."),
-    D("D", "Duplicate", "is a duplicate."),
+    C("C", "Not clear", "is not clear or the image is not sharp."),
+    D("D", "Duplicate", "is a duplicate and a similar receipt already exists."),
     E("E", "Not English", "is not in English."),
     G("G", "Good Document", "is not Rejected."),
     M("M", "Missing required data", "is missing required data like business name, details of receipts."),
-    V("V", "Not valid", "is not a receipt.");
+    V("V", "Not valid", "is not a valid receipt.");
 
     private final String name;
     private final String description;
