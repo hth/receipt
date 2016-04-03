ce302b5df23ce104d0f5417f98d6d2cd16ac2e18
Hitender Tholia <tholiahitender@gmail.com>
2016-04-03 00:22:58 -0700

    Updated text message

---
+++ common/src/test/java/com/receiptofi/utils/ScrubbedInputTest.java
@@ -0,0 +1,17 @@
+package com.receiptofi.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * User: hitender
+ * Date: 4/3/16 12:12 AM
+ */
+public class ScrubbedInputTest {
+    @Test
+    public void getText() throws Exception {
+
+    }
+
+}
