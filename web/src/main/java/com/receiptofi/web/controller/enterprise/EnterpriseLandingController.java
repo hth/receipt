2418601068651b882a337e6d9ea7da518de27e2c
hth <hth@users.noreply.github.com>
2016-06-02 16:12:12 -0700

    All set to develop business dashboard

---
+++ web/src/main/java/com/receiptofi/web/controller/enterprise/EnterpriseLandingController.java
@@ -0,0 +1,20 @@
+package com.receiptofi.web.controller.enterprise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * For ReceiptUser Employees.
+ * User: hitender
+ * Date: 5/13/16 1:34 PM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@Controller
+@RequestMapping (value = "/enterprise")
+public class EnterpriseLandingController {
+}
