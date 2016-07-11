705ee3a840cafd17f27f91d56865ee35142fc1ef
hth <hth@users.noreply.github.com>
2016-07-11 14:39:25 -0700

    added integration test

---
+++ loader/src/integration/java/com/receiptofi/loader/scheduledtasks/MailProcessITest.java
@@ -0,0 +1,33 @@
+package com.receiptofi.loader.scheduledtasks;
+
+import com.receiptofi.ITest;
+
+import org.junit.Before;
+
+/**
+ * User: hitender
+ * Date: 7/11/16 12:57 PM
+ */
+public class MailProcessITest extends ITest {
+
+    private MailProcess mailProcess;
+
+    @Before
+    public void setup() {
+        String appendToSubject = "::" + properties.getProperty("braintree.environment") + ":: ";
+        String baseDirectory = this.getClass().getResource(".").getPath().split("classes")[0];
+
+        mailProcess = new MailProcess(
+                properties.getProperty("do.not.reply.email"),
+                properties.getProperty("email.address.name"),
+                properties.getProperty("dev.sent.to"),
+                appendToSubject + properties.getProperty("mail.invite.subject"),
+                baseDirectory + "resources/test/smallGoogle.jpg",
+                baseDirectory + "resources/test/googlePlay151x47.jpg",
+                baseDirectory + "resources/test/smallFacebook.jpg",
+                baseDirectory + "resources/test/app-store151x48.jpg",
+                mailSender,
+                mailManager
+        );
+    }
+}
