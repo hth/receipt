2821b82e5bac18d7e13412a1a03cdda22eb2d833
hth <hth@users.noreply.github.com>
2016-09-11 16:47:22 -0700

    Add country short name for split expenses

--- domain/src/main/java/com/receiptofi/repository/SplitExpensesManager.java
+++ domain/src/main/java/com/receiptofi/repository/SplitExpensesManager.java
@@ -43,4 +43,6 @@ public interface SplitExpensesManager extends RepositoryManager<SplitExpensesEnt
      * @return
      */
     boolean hasSettleProcessStarted(String rdid);
+
+    List<SplitExpensesEntity> getAll();
 }
