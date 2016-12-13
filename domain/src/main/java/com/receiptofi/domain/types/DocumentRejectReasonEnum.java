2a9e44714c476e982cecf5465a84c168785f7cb8
hth <hth@users.noreply.github.com>
2016-12-12 16:15:26 -0800

    increased to 3 weeks

--- domain/src/main/java/com/receiptofi/domain/types/DocumentRejectReasonEnum.java
+++ domain/src/main/java/com/receiptofi/domain/types/DocumentRejectReasonEnum.java
@@ -12,7 +12,7 @@ public enum DocumentRejectReasonEnum {
     M("M", "Missing required data", "is missing required data like business name, address, phone, details of receipts."),
     V("V", "Not valid", "is not a valid receipt."),
     T("T", "Multiple receipts and totals", "contains multiple receipts in image."),
-    O("O", "More than 2 weeks old", "is older than 2 weeks.");
+    O("O", "More than 3 weeks old", "is older than 3 weeks.");
 
     private final String name;
     private final String description;
