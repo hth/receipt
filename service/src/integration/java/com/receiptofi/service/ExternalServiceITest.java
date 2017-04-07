0d07afe2b36e115cfc6b209e3a4bfdd3bce90ea1
hth <hth@users.noreply.github.com>
2017-04-07 19:58:37 +0530

    updated address

--- service/src/integration/java/com/receiptofi/service/ExternalServiceITest.java
+++ service/src/integration/java/com/receiptofi/service/ExternalServiceITest.java
@@ -19,13 +19,13 @@ public class ExternalServiceITest extends ITest {
     @Test
     public void getGeocodingResultNotValidAddress() {
         String address = "OR Tambo Airport Rd, Level 2, Domtex Building, OR Tambo International Airport, Johannesburg, 1627, South Africa";
-        String formattedAddress = "O.R. Tambo International Airport, OR Tambo International Airport, OR Tambo Airport Rd, Johannesburg, 1627, South Africa";
+        String formattedAddress = "O.R. Tambo International Airport, O R Tambo Airport Rd, O.R. Tambo, Johannesburg, 1627, South Africa";
         GeocodingResult[] geocodingResults = externalService.getGeocodingResults(address);
-        Assert.notEmpty(geocodingResults);
+        Assert.notEmpty(geocodingResults, "Geocoding Result cannot be empty");
         DecodedAddress decodedAddress = DecodedAddress.newInstance(geocodingResults, address);
 
-        Assert.isTrue(28.2295904 == geocodingResults[0].geometry.location.lng, "Not matching lng");
-        Assert.isTrue(-26.1325203 == geocodingResults[0].geometry.location.lat, "Not matching lat");
+        Assert.isTrue(28.2411459 == geocodingResults[0].geometry.location.lng, "Not matching lng");
+        Assert.isTrue(-26.1366728 == geocodingResults[0].geometry.location.lat, "Not matching lat");
         Assert.isTrue("ZA".equals(decodedAddress.getCountryShortName()), "Country short name is not MY");
 
         Assert.isTrue(formattedAddress.equals(decodedAddress.getFormattedAddress()), "Formatted address matches");
@@ -37,7 +37,7 @@ public class ExternalServiceITest extends ITest {
         String address = "Lot F7, 1st Floor, Bangsar Shopping Centre, No 1, Jln Tetawi 1, Bangsar Baru 59700 K Lumpur";
         String formattedAddress = "2, Jalan Telawi 1, Bangsar Baru, 59100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur, Malaysia";
         GeocodingResult[] geocodingResults = externalService.getGeocodingResults(address);
-        Assert.notEmpty(geocodingResults);
+        Assert.notEmpty(geocodingResults, "Geocoding Result cannot be empty");
         DecodedAddress decodedAddress = DecodedAddress.newInstance(geocodingResults, address);
 
         Assert.isTrue(101.6708411 == geocodingResults[0].geometry.location.lng, "Not matching lng");
