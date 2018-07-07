b980f18d42c2bedaf8744acf71c054ea1871c5c0
hth <hth@users.noreply.github.com>
2018-07-07 22:03:00 +0530

    Revert "renamed file for Jacoco"
    This reverts commit 2efc33d7b48cdd2e97fddd29f0931457038c48df.

--- view/src/main/java/com/receiptofi/web/controller/access/ExpensofiController.java
+++ view/src/main/java/com/receiptofi/web/controller/access/ExpensofiController.java
@@ -60,8 +60,8 @@ import java.util.List;
 })
 @RestController
 @RequestMapping (value = "/access/expensofi")
-public class ExpensofiReportController {
-    private static final Logger LOG = LoggerFactory.getLogger(ExpensofiReportController.class);
+public class ExpensofiController {
+    private static final Logger LOG = LoggerFactory.getLogger(ExpensofiController.class);
 
     @Value ("${expensofiReportLocation}")
     private String expensofiReportLocation;
@@ -76,7 +76,7 @@ public class ExpensofiReportController {
     private final FtpService ftpService;
 
     @Autowired
-    public ExpensofiReportController(
+    public ExpensofiController(
             @Value ("${aws.s3.bucketName}")
             String bucketName,
 
